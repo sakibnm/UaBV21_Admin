4fcafecfbb2d5629d67a51697e3731e85e60e579
mmiazi <sakibnm@gmail.com>
2018-11-15 16:09:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mmiazi/uabv21_admin/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mmiazi.uabv21_admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider);
+
+        setTitle("Users as Beacons Admin");
+    }
+}
